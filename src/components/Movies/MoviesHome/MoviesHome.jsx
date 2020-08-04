fb22a431d5384e6cee73b0d9c999b1bef5f55a61
mrnguyener21 <victortnguyen.workemail@gmail.com>
2020-08-03 23:19:52 -0700

    implemented react slick however experiencing small bugs

--- src/components/Movies/MoviesHome/MoviesHome.jsx
+++ src/components/Movies/MoviesHome/MoviesHome.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable import/no-unresolved */
 /* eslint-disable camelcase */
 /* eslint-disable import/named */
 /* eslint-disable no-console */
@@ -5,8 +7,15 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+import Slider from 'react-slick';
+
 import { MoviesNavbar } from '../..';
 import { fetchDiscoverMovies, fetchPopularMovies, fetchNowPlayingMovies, fetchTopRatedMovies, fetchUpComingMovies } from '../../../actions';
+
 import styles from './MoviesHome.module.scss';
 
 const MoviesHome = () => {
@@ -16,14 +25,34 @@ const MoviesHome = () => {
   useEffect(() => {
     dispatch(fetchUpComingMovies());
   }, []);
+
+  const settings = {
+    // dots: true,
+    arrow: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    className: 'slides',
+
+  };
   return (
     <div>
       <MoviesNavbar />
       {console.log(movies)}
+
       <div className={styles.movieList}>
-        {movies.map(({ title, poster_path }) => (
-          poster_path ? <img alt={title} className={styles.image} src={`https://image.tmdb.org/t/p/w500/${poster_path}`} /> : null
-        ))}
+        <Slider {...settings}>
+          {movies.map(({ poster_path }) => (
+            poster_path
+              ? (
+                <div className={styles.imageContainer}>
+                  <img alt={poster_path} className={styles.image} src={`https://image.tmdb.org/t/p/w500/${poster_path}`} />
+                </div>
+              )
+              : null
+          ))}
+        </Slider>
       </div>
     </div>
   );
