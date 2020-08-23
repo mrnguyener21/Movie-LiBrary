d2151d1834861cdc0b970f78cf7ecee1d908e2a0
mrnguyener21 <victortnguyen.workemail@gmail.com>
2020-08-23 16:44:37 -0700

    recommendation items are now viewable on the browser

--- src/components/Movies/MovieInfo/MovieRecommendation/MovieRecommendation.jsx
+++ src/components/Movies/MovieInfo/MovieRecommendation/MovieRecommendation.jsx
@@ -1,12 +1,35 @@
+/* eslint-disable camelcase */
 import React from 'react';
+import Rating from '@material-ui/lab/Rating';
+import Box from '@material-ui/core/Box';
 import { useSelector } from 'react-redux';
+import styles from './MovieRecommendation.module.scss';
 
 const MovieRecommendation = () => {
   const recommendation = useSelector((state) => state.movieRecommendation);
   console.log(recommendation);
   return (
-    <div>
-      <h1>movie recommendations</h1>
+    <div className={styles.container}>
+      {
+
+      recommendation
+        ? recommendation.map(({ title, poster_path, vote_average }) => (
+          poster_path && title
+            ? (
+
+              <div className={styles.movie}>
+                <img className={styles.poster} alt={poster_path} src={`https://image.tmdb.org/t/p/w500/${poster_path}`} />
+                <h4 className={styles.title}>{title}</h4>
+                <Box component="fieldset" mb={3} borderColor="transparent">
+                  <Rating className={styles.rating} name="read-only" value={(vote_average / 2)} readOnly />
+                </Box>
+              </div>
+            )
+
+            : null
+        ))
+        : null
+      }
     </div>
   );
 };
--- src/reducers/movieRecommendation.js
+++ src/reducers/movieRecommendation.js
@@ -1,8 +1,8 @@
 import { MOVIE_RECOMMENDATION } from '../constants/actionType';
 
-export default (state = {}, action) => {
+export default (state = [], action) => {
   switch (action.type) {
-    case MOVIE_RECOMMENDATION: return { ...state, ...action.payload };
+    case MOVIE_RECOMMENDATION: return [...action.payload];
 
     default: return state;
   }
